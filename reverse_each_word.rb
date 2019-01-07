8aead309c5053895abe971271af994e881cfdb1d
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-07 19:35:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
-  string.split(' ').collect {|word| word.reverse}.join(" ")
+  string.split(' ').collect {|word| word.reverse!}.join(" ")
 #  array = string.split(' ')
 #  [array.each {|word| word.reverse!}].join(" ")
 end
